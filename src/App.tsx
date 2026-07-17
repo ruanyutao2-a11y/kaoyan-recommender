4731100df676de5fc1d6a7e3ac6118908b2d1cc4
Developer <dev@kaoyan.local>
2026-07-17 18:12:06 +0800

    feat: register ComparePage route at /compare/:id

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import EvaluatingPage from './pages/EvaluatingPage'
 import PreviewPage from './pages/PreviewPage'
 import ResultPage from './pages/ResultPage'
 import RedeemPage from './pages/RedeemPage'
+import ComparePage from './pages/ComparePage'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/evaluating/:id" element={<EvaluatingPage />} />
         <Route path="/result/:id/preview" element={<PreviewPage />} />
         <Route path="/result/:id" element={<ResultPage />} />
+        <Route path="/compare/:id" element={<ComparePage />} />
         <Route path="/redeem" element={<RedeemPage />} />
     </Routes>
   )
